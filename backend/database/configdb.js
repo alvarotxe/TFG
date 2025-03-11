5129cba0e4a1252ee63ebdd2f29f3368092c7021
amc337 <amc337@gcloud.ua.es>
2025-03-11 11:28:36 +0100

    Cambios en el .env y en el confgidb

--- backend/database/configdb.js
+++ backend/database/configdb.js
@@ -1,17 +1,24 @@
+require('dotenv').config(); // Asegúrate de instalar dotenv con `npm install dotenv`
+
 const mysql = require('mysql');
 
-// Configuración de la conexión a la base de datos
+// Configuración de conexión con variables separadas
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
 
-    const connection = mysql.createConnection(process.env.DBCON);
-
-    connection.connect(function(err) {
-        if (err) {
-          console.error('Error conectando a la base de datos: ' + err.stack);
-          return;
-        }
-        console.log('Conectado a la base de datos con el ID ' + connection.threadId);
-      });
-
+connection.connect(err => {
+    if (err) {
+        console.error('Error conectando a la base de datos: ' + err.stack);
+        return;
+    }
+    console.log('Conectado a la base de datos con el ID ' + connection.threadId);
+});
 
 module.exports = connection;
+
 
